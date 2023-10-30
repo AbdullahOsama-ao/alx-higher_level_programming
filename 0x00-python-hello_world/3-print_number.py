3916faf965564c492dd10d5ab1c15ad2e3ec7e4f
AbdullahOsama-ao <abdullahosama.aoao@gmail.com>
2023-10-30 14:27:21 +0200

    mandatory & Optional tasks

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-value = 98
-print(f"{value:d} Battery street")
+number = 98
+print(f"{number:d} Battery street")
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-literal = "Holberton School"
-print("{}".format(literal) * 3)
-print(literal[:9])
+str = "Holberton School"
+print("{}".format(str) * 3)
+print(str[:9])
