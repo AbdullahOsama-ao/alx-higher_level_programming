d5df40fcf0189a470e3847371abcd03bd406807f
AbdullahOsama-ao <abdullahosama.aoao@gmail.com>
2023-10-31 15:12:24 -0700

    executale

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for x in range(0, 100):
-        print("{:02d}".format(x), end="")
-        print(("\n", ", ")[x != 99], end="")
 
+for x in range(0, 100):
+    print("{:02d}".format(x), end="")
+    print((", " if x != 99 else "\n"), end="")
