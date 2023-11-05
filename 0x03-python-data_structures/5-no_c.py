917879904c7dfb9063229631118e62654002025f
AbdullahOsama-ao <abdullahosama.aoao@gmail.com>
2023-11-05 04:44:18 -0800

    adjusting style of task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,5 @@ def no_c(my_string):
     new_string = ""
     for i in range(strlen):
         if my_string[i] != 'c' and my_string[i] != 'C':
-                new_string += my_string[i]
+            new_string += my_string[i]
     return (new_string)
